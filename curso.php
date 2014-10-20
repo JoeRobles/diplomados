61a4d75789bc547b6c818dba4ddad74e3518a397
Juan Adolfo Maldonado Polo <jmaldonado@scitechperu.com>
2014-10-20 17:36:19 -0500

    form

--- curso.php
+++ curso.php
@@ -1,7 +1,7 @@
 <?php
 $remitente = $_POST['email'];
 $destinatario = 'jmaldonado@scitechperu.com'; // en esta línea va el mail del destinatario, puede ser una cuenta de hotmail, yahoo, gmail, etc
-$asunto = 'Consulta desde la Web'; // acá se puede modificar el asunto del mail
+$asunto = 'Inscripción de curso desde la Web'; // acá se puede modificar el asunto del mail
 if (!$_POST){
 ?>
 
@@ -11,9 +11,10 @@ if (!$_POST){
     $cuerpo .= "Nombre: " . $_POST["nombre"] . "\r \n"; 
     $cuerpo .= "Email: " . $_POST["email"] . "\r \n";
     $cuerpo .= "Telefono: " . $_POST["telefono"] . "\r \n";
-    $cuerpo .= "Curso: " . $_POST["curso"] . "\r \n";
+    $cuerpo .= "Curso: " . $_POST["GrupoOpciones1"] . "\r \n";
 	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
 	// Si se agrega un campo al formulario, hay que agregarlo acá.
+
 
     $headers  = "MIME-Version: 1.0\n";
     $headers .= "Content-type: text/plain; charset=utf-8\n";
