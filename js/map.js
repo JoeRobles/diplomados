b4f7c8631d67f5ebf194a18ecc75d632a81304d1
Joe Robles <joe.robles.pdj@gmail.com>
2014-10-24 09:22:28 -0500

    map optimized

--- js/map.js
+++ js/map.js
@@ -2,11 +2,9 @@ var map;
 function initialize() {
     var mapOptions = {
         zoom: 15,
-        center: new google.maps.LatLng(-12.044156,-77.036706),
+        center: new google.maps.LatLng(-12.044156, -77.036706),
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
